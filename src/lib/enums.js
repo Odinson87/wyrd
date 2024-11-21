55e8696588b380bbddb04396dc70aa48eb469970
Paul Crawford <pcrawford66@gmail.com>
2024-11-21 12:38:57 +0000

    add new groups and types

--- src/lib/enums.js
+++ src/lib/enums.js
@@ -1,13 +1,19 @@
 import Tag from './Tag';
 
 export const TypesEnum = Object.freeze({
+    Todo: new Tag("Todo", "Todo", "#B18FDD"),
     Activity: new Tag("Activity", "Activity", "#44BBFF"),
     Trigger:  new Tag("Trigger", "Trigger", "#FF6000")
 });
 
 export const GroupsEnum = Object.freeze({
     Reward: new Tag("Reward", "Activity", "#99EE00"),
-    Garden: new Tag("Garden", "Activity", "#2F7B00")
+    Garden: new Tag("Garden", "Activity", "#BBD686"),
+    Project: new Tag("Project", "Activity", "#348aa7"),
+    Chore: new Tag("Chore", "Activity", "#D999B9"),
+    Shopping: new Tag("Shopping", "Activity", "#C57B57"),
+    Health: new Tag("Health", "Activity", "#179889"),
+    Distraction: new Tag("Distraction", "Activity", "#FFCA3A")
 });
 
 export const DurationEnum = Object.freeze({
